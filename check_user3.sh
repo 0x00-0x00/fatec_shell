88cb991f3414c421aea57365cf5c4cb888d9486f
shemhazai <andre.marques@fatec.sp.gov.br>
2017-02-15 20:25:50 -0200

    Meu script que vai ser utilizado na tarefa.

---
+++ check_user3.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#  Script para encontrar informacoes sobre usuarios
+#   Tarefa da Fatec
+#   Escrito por Andre Marques - 3o seg
+
+
+
+user_file="/etc/passwd";
+
+if [[ $1 == "" ]]; then
+    echo -n "Procurar por usuario: ";
+    read user;
+else
+    user=$1;
+fi
+
+function check_user
+{
+    if [[ ! -e $user_file ]]; then
+        echo "[!] Nao consegui encontrar o arquivo '$user_file'";
+        exit;
+    fi
+
+    num_users=$(cat $user_file | grep $1 | wc -l)
+    if [[ $num_users -eq 0 ]]; then
+        echo "Nao foram encontrados usuarios com o nome '$1'.";
+        exit;
+    fi
+
+
+    data=$(cat $user_file | grep $1 | grep -oP '.+(?=::)');
+    user=$(echo $data | grep -oP '[a-zA-Z0-9]+(?=:x:)');
+    uid=$(echo $data | grep -oP '(?!:x:)[0-9]+(?=:)');
+    gid=$(echo $data | grep -oP '(?![0-9]+:)[0-9]+');
+
+    echo "_______________________________"
+    echo "    Informacoes de usuario"
+    echo "=============================="
+
+    echo -n "User: ";
+    echo $user;
+
+    echo -n "UID: ";
+    echo $uid;
+
+    echo -n "GID: ";
+    echo $gid;
+
+}
+
+check_user $user
